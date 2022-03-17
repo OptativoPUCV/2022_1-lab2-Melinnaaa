bddaaa414ed77c043a87496c4feb97cacce3a29d
Your Name <you@example.com>
2022-03-17 17:52:05 -0300

    new try

--- list.c
+++ list.c
@@ -52,6 +52,7 @@ void* firstList(List* list)
 
 void* nextList(List* list) 
 {
+    if (!list->current) return NULL;
     if (list->current->next != NULL)
     {
         list->current = list->current->next;
@@ -72,7 +73,7 @@ void* lastList(List* list)
 
 void* prevList(List* list) 
 {
-    if(list->current->prev != NULL)
+    if(list->current!=NULL && list->current->prev != NULL)
     {
         list->current = list->current->prev;
         return list->current->data;
@@ -121,8 +122,11 @@ void * popBack(List * list)
 void * popCurrent(List * list) 
 {
     void* dato = list->current->data;
-    list->current->prev->next = list->current->next;
-    list->current->next->prev = list->current->prev;
+    if(list->current->prev)
+      list->current->prev->next = list->current->next;
+    if(list->current->next)
+      list->current->next->prev = list->current->prev;
+    
     free(list->current);
     return dato;
 }
